c090bf20cc418e53a28fc029b0332d77fdad24f7
Kasper Fuglsang <kfu@dbc.dk>
2017-08-25 10:02:43 +0200

    initial creation of react components

---
+++ src/client/components/profile/CreateProfile.container.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class Login extends React.Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        create profile
+      </div>
+    );
+  }
+}
