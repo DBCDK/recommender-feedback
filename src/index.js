44929a1d77f8eba891765906b038ae4f31db0106
Kasper Fuglsang <kfu@dbc.dk>
2017-08-25 12:29:04 +0200

    remove redundant App.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './client/App';
+import App from './client/app';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './client/reducers/reducer'
