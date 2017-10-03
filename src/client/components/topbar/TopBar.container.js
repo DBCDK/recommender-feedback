b1f7163526cdbe01017e49830ec1f93664aae5ca
Kasper Fuglsang <kfu@dbc.dk>
2017-10-03 10:08:24 +0200

    added back-btn to top-bar when on feedback-page

--- src/client/components/topbar/TopBar.container.js
+++ src/client/components/topbar/TopBar.container.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {ON_LOGOUT_REQUEST} from '../../redux';
+import {ON_LOGOUT_REQUEST, HISTORY_REPLACE} from '../../redux';
 
 class TopBar extends React.Component {
 
@@ -10,11 +10,25 @@ class TopBar extends React.Component {
 
   render() {
     const user = this.props.profileState.user || 'some@email.dk';
+    let backBtn = null;
+    if (this.props.routerState.path === '/feedback') {
+      backBtn = <span className='back-btn' onClick={() => {
+        this.props.dispatch({type: HISTORY_REPLACE, path: '/søg'});
+      }}>
+        <span className='glyphicon glyphicon-chevron-left'/>
+        {'FIND BOG'}
+      </span>;
+    }
     return (
       this.props.profileState.isLoggedIn &&
-      <div className="top-bar row text-right">
-        <span className="username">{user}</span>
-        <span className="logout-btn" onClick={this.onLogout}>log ud</span>
+      <div className='top-bar row'>
+        <div className="text-left col-xs-4">
+          {backBtn}
+        </div>
+        <div className="text-right col-xs-8">
+          <span className="username">{user}</span>
+          <span className="logout-btn" onClick={this.onLogout}>log ud</span>
+        </div>
       </div>
     );
   }
@@ -24,6 +38,6 @@ class TopBar extends React.Component {
 export default connect(
   // Map redux state to group prop
   (state) => {
-    return {profileState: state.profileReducer};
+    return {profileState: state.profileReducer, routerState: state.routerReducer};
   }
 )(TopBar);
