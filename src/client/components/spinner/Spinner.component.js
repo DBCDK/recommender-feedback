1bed6229c2350f03434c01d73251341d94ad0840
Kasper Fuglsang <kfu@dbc.dk>
2017-10-23 13:58:24 +0200

    fix spinner issue, messing up scroll bars

--- src/client/components/spinner/Spinner.component.js
+++ src/client/components/spinner/Spinner.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Spinner extends React.Component {
   render() {
-    return <div style={{textAlign: 'center'}}><div style={{borderLeftColor: 'black'}} className="spinner">Loading...</div></div>;
+    return <div style={{textAlign: 'center'}}><span style={{borderLeftColor: 'black'}} className="spinner"></span></div>;
   }
 }
 
