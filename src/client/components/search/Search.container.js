258cbcf13769d362667354d7cf3019e4bf424572
Kasper Fuglsang <kfu@dbc.dk>
2017-10-24 15:52:56 +0200

    added suggestions dropdown

--- src/client/components/search/Search.container.js
+++ src/client/components/search/Search.container.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {ON_SEARCH_REQUEST, HISTORY_PUSH, ON_RECOMMEND_REQUEST} from '../../redux';
+import {ON_SEARCH_REQUEST, ON_SUGGEST_REQUEST, HISTORY_PUSH, ON_RECOMMEND_REQUEST} from '../../redux';
 import Spinner from '../spinner/Spinner.component';
+
+const MIN_SUGGEST_QUERY_LENGTH = 3;
 
 const WorkList = (props) => {
   return (
@@ -33,6 +35,9 @@ const WorkRow = (props) => {
 };
 
 const SearchForm = (props) => {
+  const numSuggestions = props.suggestions ? props.suggestions.titles.length + props.suggestions.creators.length : 0;
+  const showSuggestions = props.focusValue && numSuggestions > 0;
+
   return (
     <div className='row search-form'>
       <form onSubmit={event => {
@@ -50,8 +55,63 @@ const SearchForm = (props) => {
                 props.onQueryChange(e.target.value);
               }
             }
+            onKeyDown={e => {
+              if (e.keyCode === 38) {
+                // up
+                if (showSuggestions) {
+                  props.onHoverChange(Math.max(0, props.hoverIndex -1));
+                }
+                e.preventDefault();
+              }
+              else if (e.keyCode === 40) {
+                // down
+                if (showSuggestions) {
+                  props.onHoverChange(Math.min(numSuggestions-1, props.hoverIndex +1));
+                }
+                e.preventDefault();
+              }
+            }}
+            onFocus={props.onFocus}
+            onBlur={props.onBlur}
             value={props.value}
+            autoComplete='off'
           />
+          {showSuggestions && <div className='autocomplete panel panel-default drop-shadow col-xs-8'>
+            {props.suggestions.titles.length > 0 &&
+              <div>
+                <h4>Titler</h4>
+                <ul>
+                  {props.suggestions.titles.map((s, idx) => {
+                    const className = props.hoverIndex === idx ? 'hover' : null;
+                    return <li
+                      onMouseOver={() => {
+                        props.onHoverChange(idx);
+                      }}
+                      onMouseDown={() => {
+                        props.onQueryChange(s.term);
+                        props.onSearch();
+                      }} className={className} key={s.term+idx}>{s.term}</li>;
+                  })}
+                </ul>
+              </div>}
+            {props.suggestions.creators.length > 0 &&
+              <div>
+                <h4>Forfattere</h4>
+                <ul>
+                  {props.suggestions.creators.map((s, idx) => {
+                    const className = props.hoverIndex === idx + props.suggestions.titles.length ? 'hover' : null;
+                    return <li
+                      onMouseOver={() => {
+                        props.onHoverChange(idx + props.suggestions.titles.length);
+                      }}
+                      onMouseDown={() => {
+                        props.onQueryChange(s.term);
+                        props.onSearch();
+                      }} className={className} key={s.term+idx}>{s.term}</li>;
+                  })}
+                </ul>
+              </div>}
+          </div>}
         </div>
         <div className='col-sm-2'>
           <button type="submit" className="btn btn-primary btn-block">
@@ -68,7 +128,9 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      focusValue: false,
+      hoverIndex: -1
     };
   }
 
@@ -82,7 +144,10 @@ class Search extends React.Component {
   };
 
   onQueryChange = (query) => {
-    this.setState({query});
+    if (query.length >= MIN_SUGGEST_QUERY_LENGTH) {
+      this.props.dispatch({type: ON_SUGGEST_REQUEST, query: this.state.query});
+    }
+    this.setState({query, hoverIndex: -1});
   };
 
   render() {
@@ -93,7 +158,21 @@ class Search extends React.Component {
           <SearchForm
             onSearch={this.onSearch}
             onQueryChange={this.onQueryChange}
-            value={this.state.query}/>
+            value={this.state.query}
+            suggestions={this.state.query.length >= MIN_SUGGEST_QUERY_LENGTH ? this.props.searchState.suggestions : null}
+            hoverIndex={this.state.hoverIndex}
+            onFocus={() => {
+              this.setState({searchFocus: true});
+            }}
+            onBlur={() => {
+              this.setState({searchFocus: false});
+            }}
+            focusValue={this.state.searchFocus}
+            onHoverChange={hoverIndex => {
+              const allSuggestions = [...this.props.searchState.suggestions.titles, ...this.props.searchState.suggestions.creators];
+              const query = allSuggestions[hoverIndex].term;
+              this.setState({hoverIndex, query});
+            }}/>
           {this.props.searchState.isFetching && <Spinner/>}
           {this.props.searchState.works && this.props.searchState.works.length === 0 && <h3>Søgning gav tomt resultat</h3>}
           {this.props.searchState.works && <WorkList works={this.props.searchState.works} onWorkSelect={this.onWorkSelect}/>}
